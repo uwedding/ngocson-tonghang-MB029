ff9ca2e71b59f31c37654f4adfbe442b99c49c42
cuongnv26 <cuongnv26@msb.com.vn>
2025-12-04 13:43:44 +0700

    update code

--- data.js
+++ data.js
@@ -310,7 +310,7 @@ window.event_data = {
             hoverColor: "",
             id: "owj34amy",
             target:
-              "./I Do - 911 (Lyrics & Vietsub) - Fall In Luv (youtube).mp3",
+              "./Em Đồng Ý  I Do  - Đức Phúc x 911 [Lyrics Video]  Điều anh ước muốn bây giờ ngay đây rồi - Mưa. (youtube).mp3",
             type: "click",
           },
           {
@@ -593,7 +593,7 @@ window.event_data = {
             nameFile:
               "EM ĐỒNG Ý (I DO) - ĐỨC PHÚC x 911 x KHẮC HƯNG - OFFICIAL MUSIC VIDEO - VALENTINE 2023 (mp3cut.net).mp3",
             target:
-              "./I Do - 911 (Lyrics & Vietsub) - Fall In Luv (youtube).mp3",
+              "./Em Đồng Ý  I Do  - Đức Phúc x 911 [Lyrics Video]  Điều anh ước muốn bây giờ ngay đây rồi - Mưa. (youtube).mp3",
             type: "click",
           },
           {
@@ -635,7 +635,7 @@ window.event_data = {
             nameFile:
               "EM ĐỒNG Ý (I DO) - ĐỨC PHÚC x 911 x KHẮC HƯNG - OFFICIAL MUSIC VIDEO - VALENTINE 2023 (mp3cut.net).mp3",
             target:
-              "./I Do - 911 (Lyrics & Vietsub) - Fall In Luv (youtube).mp3",
+              "./Em Đồng Ý  I Do  - Đức Phúc x 911 [Lyrics Video]  Điều anh ước muốn bây giờ ngay đây rồi - Mưa. (youtube).mp3",
             type: "click",
           },
           {
